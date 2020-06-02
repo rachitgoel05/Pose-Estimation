19db531109d9d46abd968009b3b2ed67f0036a2b
rachitgoel <rachit0505@gmail.com>
2020-06-02 15:49:53 +0530

    commit

--- main_script.py
+++ main_script.py
@@ -6,42 +6,6 @@ cwd=os.getcwd()
 
 src_dir = (cwd+'/videos/outputs/')
 dst_dir = (cwd+'/RESULTS/')
-#video_1='P1_7.mp4'
-#video_2='P3_2.mp4'
-#os.system('python demo_video.py --video '+video_1+' --frame_ratio 5 --process_speed 1 --end 15')
-#os.rename((cwd+'/videos/outputs/homealone1.jpg'),(cwd+'/videos/outputs/1.jpg'))
-#
-#'''---------------WILL SAVE THE IMAGE BY FIRST IMAGE IN THE DIRECTORY--------'''
-#import glob
-#import shutil
-#
-#for jpgfile in glob.iglob(os.path.join(src_dir, "1.jpg")):
-#    shutil.copy(jpgfile, dst_dir)
-#
-#os.system('python demo_video.py --video '+video_2+' --frame_ratio 5 --process_speed 1 --end 15')
-#os.chdir(cwd+'/videos/outputs/')
-#os.rename((cwd+'/videos/outputs/homealone1.jpg'),(cwd+'/videos/outputs/2.jpg'))
-#
-#'''---------------WILL SAVE THE IMAGE BY FIRST SECOND IN THE DIRECTORY--------'''
-#
-#src_dir = (cwd+'/videos/outputs/')
-#dst_dir = (cwd+'/RESULTS/')
-#for jpgfile in glob.iglob(os.path.join(src_dir, "2.jpg")):
-#    shutil.copy(jpgfile, dst_dir)    
-#
-#'''------------TO MERGE TWO IMAGES PRESENT IN THE DIRECTORY----------'''
-#
-#import cv2
-#import numpy as np
-#img1=cv2.imread(dst_dir+"/1_1_1.jpg")
-#img2=cv2.imread(dst_dir+"/1_2_2.jpg")
-#img1 = cv2.resize(img1, (1000,700), None,0.25,0.25)
-#img2 = cv2.resize(img2, (1000,700), None,0.25,0.25)
-#
-#hor=np.hstack((img1,img2))
-#im_h=cv2.hconcat([img1,img2])
-#cv2.imwrite(dst_dir+"processing.jpg",im_h)
-
 '''----------------TO PROCESS THE COMBINED IMAGE---------'''
 os.chdir(cwd)
 os.system('python demo_image.py --image ./RESULTS/processing2.jpg')
